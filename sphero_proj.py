e9e940ebf8c5cf0cfa5cf52ad0319b7ef2cea3bb
_michdev <gamedev.mich@gmail.com>
2025-09-10 11:41:08 -0400

    (mostly) finished script, testing next

--- sphero_proj.py
+++ sphero_proj.py
@@ -1,12 +1,14 @@
 import time
+import math
+import asyncio
 from spherov2 import scanner
 from spherov2.sphero_edu import SpheroEduAPI
 from spherov2.types import Color
 from enum import Enum
 
-toy = scanner.find_toy()
-
+toy = scanner.find_Mini()
 #region const settings
+
 FORCE_LIGHT_RANGE = {"min": 0, "max": 255}
 FORCE_SPEED_RANGE = {"min": 0, "max": 255}
 
@@ -25,24 +27,49 @@ class States(Enum):
     CHOOSE_SPEED = 2
     MOVE         = 3
 
-state = States.IDLE
-
-
 with SpheroEduAPI(toy) as bot:
-    def set_state(s):
+
+    state = States.IDLE
+
+    def set_state(s: int):
         state = s
         if s == States.IDLE:
             return
         elif s == States.CHOOSE_ROT:
-            
-    
+            rotate()
+        elif s == States.CHOOSE_SPEED:
+            choose_speed()
+        elif s == States.MOVE:
+            move()
+
     async def rotate():
+        bot.set_back_led(255)
         while state == States.CHOOSE_ROT:
             await bot.spin(ROTATE_SPEED*TICK_DELTA, TICK_DELTA)
 
+    async def choose_speed():
+        elapsed = 0.0
+        power = 0.0
+        while state == States.CHOOSE_SPEED:
+            power = math.sin(elapsed * math.pi / 2) * 255
+            bot.set_back_led(power)
+            elapsed += TICK_DELTA
+            await asyncio.sleep(TICK_DELTA)
     
+    async def move():
+        bot.set_back_led(0)
+        speed = 255.0
+        while state == States.MOVE:
+            bot.set_speed(speed)
+            speed -= DECEL * TICK_DELTA
+            await asyncio.sleep(TICK_DELTA)
+            if speed <= 0:
+                set_state(States.CHOOSE_ROT)
+
     def on_collision(api):
         if state == States.CHOOSE_ROT:
             set_state(States.CHOOSE_SPEED)
         if state == States.CHOOSE_SPEED:
             set_state(States.MOVE)
+        if state == States.MOVE:
+            bot.spin(180, 1)
