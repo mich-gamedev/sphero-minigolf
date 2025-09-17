db124816c148e57886853ded7a7d0107e4fa156f
_michdev <gamedev.mich@gmail.com>
2025-09-17 09:42:22 -0400

    added teams

--- sphero_proj.py
+++ sphero_proj.py
@@ -1,3 +1,13 @@
+from spherov2.types import Color
+import time
+import math
+import asyncio
+import sys
+from spherov2 import scanner
+from spherov2.sphero_edu import SpheroEduAPI
+from enum import Enum
+import keyboard
+
 #region USER SETTINGS
 # How fast it rotates per second.
 ROTATE_SPEED = 720.
@@ -7,6 +17,15 @@ BOUNCE_DECAY = .9
 DECEL        = 255./2.
 # How many seconds between each update.
 TICK_DELTA   = 1./20.
+# Team colors
+TEAMS        = [
+    Color(255, 000, 000), # red
+    Color(255, 255, 000), # yellow
+    Color(000, 255, 000), # green
+    Color(000, 255, 255), # cyan
+    Color(000, 000, 255), # blue
+    Color(255, 000, 255), # magenta
+]
 #endregion
 
 
@@ -18,21 +37,14 @@ TICK_DELTA   = 1./20.
 
 
 # ---- CODE BELOW ---- #
-import time
-import math
-import asyncio
-import sys
-from spherov2 import scanner
-from spherov2.sphero_edu import SpheroEduAPI
-from spherov2.types import Color
-from enum import Enum
-import keyboard
-
 cli_args = sys.argv
 toy_name = ""
+team = 0
 for i in cli_args:
     if i.startswith("toyid="):
         toy_name = i.removeprefix("toyid=")
+    elif i.startswith("team="):
+        team = int(i.removeprefix("team="))
 
 toy = scanner.find_Mini(toy_name=(toy_name if toy_name else None))
 
@@ -46,9 +58,6 @@ state = States.CHOOSE_ROT
 
 
 with SpheroEduAPI(toy) as bot:
-    state = States.CHOOSE_ROT
-
-
     async def run():
         global state
         exit_flag = False
@@ -65,14 +74,15 @@ with SpheroEduAPI(toy) as bot:
                 else:
                     bot.spin(ROTATE_SPEED*TICK_DELTA, TICK_DELTA)
             elif state == States.CHOOSE_SPEED:
-                speed = math.cos(elapsed * math.pi * 4) * 255
+                speed = math.sin(elapsed * math.pi * 4) * 255
                 bot.set_back_led(int(speed))
-                bot.set_main_led(Color(int(speed), int(speed), int(speed)))
+                bot.set_main_led(TEAMS[team] * int(speed/255))
                 elapsed += TICK_DELTA
                 if keyboard.is_pressed('enter'):
                     state = States.MOVE
                     elapsed = 0
             elif state == States.MOVE:
+                bot.set_main_led(TEAMS[team])
                 bot.set_back_led(int(speed))
                 if speed > 0:
                     bot.set_speed(int(speed))
