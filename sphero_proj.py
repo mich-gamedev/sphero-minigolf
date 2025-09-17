0dd42148203c33d08ae20ed74438e98f255d6e8d
_michdev <gamedev.mich@gmail.com>
2025-09-17 08:23:31 -0400

    removed multithreading, got it working

--- sphero_proj.py
+++ sphero_proj.py
@@ -4,7 +4,7 @@ ROTATE_SPEED = 720.
 # How much speed it loses when running into a wall. (CURRENTLY UNUSED)
 BOUNCE_DECAY = .9
 # How much it slows down per second
-DECEL        = 255./8.
+DECEL        = 255./2.
 # How many seconds between each update.
 TICK_DELTA   = 1./20.
 #endregion
@@ -18,11 +18,13 @@ TICK_DELTA   = 1./20.
 
 
 # ---- CODE BELOW ---- #
+import time
 import math
 import asyncio
 import sys
 from spherov2 import scanner
 from spherov2.sphero_edu import SpheroEduAPI
+from spherov2.types import Color
 from enum import Enum
 import keyboard
 
@@ -41,66 +43,45 @@ class States(Enum):
     MOVE         = 3 # Performs movement
 
 state = States.CHOOSE_ROT
-last_runner = None
 
 
 with SpheroEduAPI(toy) as bot:
+    state = States.CHOOSE_ROT
 
 
-
-    def set_state(s):
+    async def run():
         global state
-        global last_runner
-        state = s
-        try:
-            last_runner
-        except NameError:
-            print("Couldn't find existing async loop. continuing")
-        else:
-            if last_runner != None:
-                last_runner.close()
-        asyncio.create_task()
-        last_runner = asyncio.Runner()
-
-        if s == States.IDLE:
-            return
-        elif s == States.CHOOSE_ROT:
-            last_runner.run(rotate())
-        elif s == States.CHOOSE_SPEED:
-            last_runner.run(choose_speed())
-        elif s == States.MOVE:
-            last_runner.run(move())
-
-#region STATE FUNCTIONS
-    async def rotate():
-        bot.set_back_led(255)
-        while state == States.CHOOSE_ROT:
+        exit_flag = False
+        elapsed: float = 0.0
+        speed: float
+        while not exit_flag:
+            if keyboard.is_pressed('esc'):
+                bot.set_speed(0)
+                exit_flag = True
+            elif state == States.CHOOSE_ROT:
+                bot.set_back_led(255)
+                if keyboard.is_pressed('space'):
+                    state = States.CHOOSE_SPEED
+                else:
+                    bot.spin(ROTATE_SPEED*TICK_DELTA, TICK_DELTA)
+            elif state == States.CHOOSE_SPEED:
+                speed = math.cos(elapsed * math.pi * 4) * 255
+                bot.set_back_led(int(speed))
+                bot.set_main_led(Color(int(speed), int(speed), int(speed)))
+                elapsed += TICK_DELTA
+                if keyboard.is_pressed('enter'):
+                    state = States.MOVE
+                    elapsed = 0
+            elif state == States.MOVE:
+                bot.set_back_led(int(speed))
+                if speed > 0:
+                    bot.set_speed(int(speed))
+                    speed -= DECEL * TICK_DELTA
+                else:
+                    state = States.CHOOSE_ROT
+                    speed = 0
             await asyncio.sleep(TICK_DELTA)
-            if keyboard.is_pressed('space'):
-                break
-            bot.spin(ROTATE_SPEED*TICK_DELTA, TICK_DELTA)
-        set_state(States.CHOOSE_SPEED)
 
-    async def choose_speed():
-        elapsed = 0.0
-        power = 0.0
-        while state == States.CHOOSE_SPEED:
-            await asyncio.sleep(TICK_DELTA)
-            if keyboard.is_pressed('enter'):
-                set_state(States.MOVE)
-                break
-            power = math.cos(elapsed * math.pi / 2) * 255
-            bot.set_back_led(power)
-            elapsed += TICK_DELTA
-    
-    async def move():
-        bot.set_back_led(0)
-        speed = 255.0
-        while state == States.MOVE:
-            bot.set_speed(speed)
-            speed -= DECEL * TICK_DELTA
-            await asyncio.sleep(TICK_DELTA)
-            if speed <= 0:
-                set_state(States.CHOOSE_ROT)
-#endregion
-    set_state(States.CHOOSE_ROT)
+    asyncio.run(run())
+
+
